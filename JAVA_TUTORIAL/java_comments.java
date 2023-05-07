ede5a5ab02d526682688bbb67a670e738dd49ae6
Diego Renato Llerena Tellez <dllerenat@unsa.edu.pe>
2023-05-07 09:45:22 -0500

    Java_Comments (Creado)

---
+++ JAVA_TUTORIAL/java_comments.java
@@ -0,0 +1,29 @@
+public class java_comments{
+    public static void main(String [] args){
+    /*Java Comments
+    Los comentarios pueden ser usados para explicar el c¢digo de Java, y para hacerlo m s comprensible.
+    Tambi‚n puede ser usado para prevenir ejecuciones al probar codigo alternativo.
+    
+    Single-line Comments
+    Los commentarios de una sola linea comienzan con 2 slashes (//)
+    Cualquier texto entre // y el final de una linea es ignorado por Java (no ser  ejecutado).
+
+    Este ejemplo usa una sola linea de comentario antes de la linea de c¢digo: */
+
+        // Este es un comentario
+        System.out.println("Hola mundo");
+    
+    // Este otro ejemplo utiliza una sola linea de comentario al final de la linea de c¢digo: 
+        
+        System.out.println("Hola mundo");//Este es un comentario
+
+    /*Java Multi-Line Comments
+    Los comentarios multi-linea comienzan con (/*) y terminan con (* /).
+    Cualquier texto entre (/*) y  (* /) ser n ignorados por Java.
+    Este ejemplo utiliza un comentario multi-linea (commentario en bloque) para explicar el c¢digo  */
+        
+        /* El c¢digo debajo imprimira las palabras Hola Mundo 
+        en la pantalla y es asombroso*/
+        System.out.println("Hola mundo");
+    }
+}
