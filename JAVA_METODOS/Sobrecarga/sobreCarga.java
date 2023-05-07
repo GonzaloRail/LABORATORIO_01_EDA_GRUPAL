5e51c1106d0e21914504bfa9e08817818c209110
Gonzalo01703 <glaymem@unsa.edu.pe>
2023-05-07 15:51:25 -0500

    Se completo el capitulo de sob recarga

---
+++ JAVA_METODOS/Sobrecarga/sobreCarga.java
@@ -0,0 +1,73 @@
+package Sobrecarga;
+
+public class sobreCarga {
+    public static void main(String[] args){
+
+/*
+ * Sobrecarga de métodos
+ * Con la sobrecarga de métodos , varios métodos pueden tener el mismo nombre con diferentes parámetros:
+ */
+
+
+    }
+
+    public static void metodo(int a){
+		
+	}
+    public static void metodo(double a){
+		
+	}
+    public static void metodo(float a, float b){
+		
+	}
+
+    //Considere el siguiente ejemplo, que tiene dos métodos que suman números de diferente tipo:
+
+/**
+ * 
+    public static int metodo(int a,int b){
+		return a+b;
+	}
+    public static double metodo(double a,double b){
+        return a+b;
+		
+	}
+    public static void main(String[] args) {
+        int myNum1 = plusMethod(8, 5);
+        double myNum2 = plusMethod(4.3, 6.26);
+        System.out.println("int: " + myNum1);
+        System.out.println("double: " + myNum2);
+      }
+
+ * 
+ * 
+ * 
+ * 
+ */
+    /*
+     * En lugar de definir dos métodos que deberían hacer lo mismo, es mejor sobrecargar uno.
+     * En el siguiente ejemplo, sobrecargamos el plusMethod método para que funcione con ambos int y double:
+     */
+    
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
