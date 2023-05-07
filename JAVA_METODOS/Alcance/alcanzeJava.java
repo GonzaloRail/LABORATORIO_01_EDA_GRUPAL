c19db422e18a56e08c6501a5d666509b8a167f2f
Gonzalo01703 <glaymem@unsa.edu.pe>
2023-05-07 15:54:51 -0500

    Se completo el capitulo de alcanze de java

---
+++ JAVA_METODOS/Alcance/alcanzeJava.java
@@ -0,0 +1,27 @@
+package Alcance;
+
+public class alcanzeJava {
+    
+    /*
+     * Alcance de Java: 
+     * En Java, solo se puede acceder a las variables dentro de la región en la que se crearon. Esto se llama alcance .
+     * Alcance del método
+     * 
+     * Las variables declaradas directamente dentro de un método están disponibles en cualquier parte del método siguiendo la línea de código en la que fueron declaradas:
+     * 
+     */
+
+     public static void main(String[] args){
+        int x=100;
+        System.out.println(x);
+
+
+        /*
+         * Ámbito de bloque
+         * Un bloque de código se refiere a todo el código entre llaves {}. 
+         * Solo se puede acceder a las variables declaradas dentro de bloques de código mediante 
+         * el código entre llaves, que sigue la línea en la que se declaró la variable:
+         */
+       
+     }
+}
