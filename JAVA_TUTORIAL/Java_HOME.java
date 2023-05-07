4892f9e840321bc2f3824ad93bbb44bfb78791c8
ahilacondo <ahilacondo@unsa.edu.pe>
2023-05-06 20:07:43 -0500

    Java_HOME

---
+++ JAVA_TUTORIAL/Java_HOME.java
@@ -0,0 +1,26 @@
+/*
+Learn Java
+
+-Java es un lenguaje de programación popular.
+
+-Java se utiliza para desarrollar aplicaciones móviles, aplicaciones web, aplicaciones de escritorio, juegos y mucho más.
+*/
+
+//Ejemplos
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
