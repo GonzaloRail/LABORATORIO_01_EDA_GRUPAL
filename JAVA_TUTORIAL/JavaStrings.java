f6f53f6f93dd4bc1c8581d051e6f6bcdbb7e9402
Mauricio Eduardo Zegarra Puma <mzegarrapu@unsa.edu.pe>
2023-05-07 00:01:51 -0500

    Primera versión del archivo Java Strings

---
+++ JAVA_TUTORIAL/JavaStrings.java
@@ -0,0 +1,8 @@
+public class JavaStrings {
+	public static void main (String args[]) {
+		String mynames = "Mauricio Eduardo";
+		System.out.println("La longitud del String \"mynames\" es: " + mynames.length());
+		System.out.println("\nUpper Case: " + mynames.toUpperCase());
+		System.out.println("\nLower Case: " + mynames.toLowerCase());
+	}
+}
